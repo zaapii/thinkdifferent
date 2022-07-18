472adc6e385cf40815047afa44cbd41b6d3d3a66
Imanol <izapico@kiusys.com>
2022-07-18 13:55:44 -0300

    Zapico Imanol | Desafio: Menu e-commerce

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -104,7 +104,6 @@ const ResponsiveAppBar = () => {
                             </Button>
                         ))}
                     </Box>
-
                     <Box sx={{flexGrow: 0}}>
                         <Tooltip title="Open settings">
                             <IconButton onClick={handleOpenUserMenu} sx={{p: 0}}>
